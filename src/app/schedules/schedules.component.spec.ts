4d508dd0fff5bea189938463bfeb44c3a3f0920a
joanasesinando <joanasesinando@tecnico.ulisboa.pt>
2020-10-13 16:11:30 +0100

    Fixing tests

--- src/app/schedules/schedules.component.spec.ts
+++ src/app/schedules/schedules.component.spec.ts
@@ -4,8 +4,7 @@ import {DebugElement} from '@angular/core';
 import {SchedulesComponent} from './schedules.component';
 
 import {RouterTestingModule} from '@angular/router/testing';
-
-import {ClassType} from '../_domain/ClassType';
+import {TranslateFakeLoader, TranslateLoader, TranslateModule} from '@ngx-translate/core';
 
 
 describe('SchedulesComponent', () => {
@@ -13,61 +12,17 @@ describe('SchedulesComponent', () => {
   let fixture: ComponentFixture<SchedulesComponent>;
   let de: DebugElement;
 
-  let data: {selectedCourses: any, academicTerm: string, degreeID: number};
-
   beforeEach(async(() => {
-    data = {
-      selectedCourses: [
-        {
-          _id: 1,
-          _name: 'Course #1',
-          _acronym: 'C1',
-          _types: [ClassType.THEORY_PT, ClassType.LAB_PT],
-          _campus: ['Alameda'],
-          _shifts: [
-            {
-              _name: 'T01',
-              _types: [ClassType.THEORY_PT],
-              _lessons: [
-                {
-                  _start: 'Mon Sep 07 2020 09:30:00 GMT+0100 (Western European Summer Time)',
-                  _end: 'Mon Sep 07 2020 11:00:00 GMT+0100 (Western European Summer Time)',
-                  _room: 'R1',
-                  _campus: 'Alameda'
-                },
-                {
-                  _start: 'Wed Sep 09 2020 09:30:00 GMT+0100 (Western European Summer Time)',
-                  _end: 'Wed Sep 09 2020 11:00:00 GMT+0100 (Western European Summer Time)',
-                  _room: 'R1',
-                  _campus: 'Alameda'
-                }
-              ],
-              _campus: 'Alameda'
-            },
-            {
-              _name: 'L01',
-              _types: [ClassType.LAB_PT],
-              _lessons: [
-                {
-                  _start: 'Tue Sep 08 2020 09:30:00 GMT+0100 (Western European Summer Time)',
-                  _end: 'Tue Sep 08 2020 11:00:00 GMT+0100 (Western European Summer Time)',
-                  _room: 'R2',
-                  _campus: 'Alameda'
-                }
-              ],
-              _campus: 'Alameda'
-            }
-          ],
-          _courseLoads: { TEORICA: 3, LABORATORIAL: 1.5 }
-        }
-      ],
-      academicTerm: '2020/2021',
-      degreeID: 1
-    };
-    window.history.pushState(data, '', '');
-
     TestBed.configureTestingModule({
-      imports: [ RouterTestingModule ],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader
+          }
+        }),
+        RouterTestingModule
+      ],
       declarations: [ SchedulesComponent ]
     })
     .compileComponents();
@@ -82,15 +37,6 @@ describe('SchedulesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should have data received', () => {
-    expect(data).toBeTruthy();
-  });
-
-  it('should return to homepage if no data', () => { // TODO
-    // window.history.state.pop();
-    // const router = TestBed.createComponent(Router);
   });
 
 });
