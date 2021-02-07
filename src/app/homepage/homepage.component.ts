240e84df7c75f72d44ba9f206f2cf256a257bf33
Joana Sesinando <joanasesinando@tecnico.ullisboa.pt>
2021-02-07 00:48:49 +0000

    Minor fix: spinners position

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -55,8 +55,8 @@ export class HomepageComponent implements OnInit, AfterViewInit {
 
   spinners = {
     academicTerm: true,
-    degree: false,
-    course: false,
+    degree: true,
+    course: true,
     loadingPage: false
   };
 
@@ -140,7 +140,7 @@ export class HomepageComponent implements OnInit, AfterViewInit {
       // Save state
       this.saveAcademicTermsState(this.academicTerms);
 
-      this.spinners.academicTerm = false;
+      // this.spinners.academicTerm = false;
     });
     this.spinners.loadingPage = false;
 
