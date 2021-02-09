b1bd70df869547e2d240d3f892e8a7a0fa2fc36d
Joana Sesinando <joanasesinando@tecnico.ullisboa.pt>
2021-02-09 01:35:02 +0000

    About modal done

--- src/app/homepage/about-modal/about-modal.component.ts
+++ src/app/homepage/about-modal/about-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-modal',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(public translateService: TranslateService) { }
 
   ngOnInit(): void {
   }
